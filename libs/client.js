71f4fd4f02db591bed9ae1bc5246759b279967d6
Toma Takeda <take.foot.14@i.softbank.jp>
2024-01-26 17:57:42 +0900

    main

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
     serviceDomain: "ysyqup4zbw",
-    apiKey: process.env.API_KEY,
+    apiKey: rmi6FiSeIV1Y3sY32TLtHRQpas0giQPpz17l,
 })
